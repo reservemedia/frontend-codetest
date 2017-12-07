1aee7ead930eddddf96d97d4814265e7ec062a9c
Tom Conroy <tom@thomasconroy.net>
2017-12-07 17:10:59 -0500

    use dev server for proxying api requests

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Module
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
+const jsonServer = require('express')();
+const path = require('path');
+const request = require('request');
 
 // File
 const config = require('./webpack.config');
@@ -8,7 +11,7 @@ const config = require('./webpack.config');
 const host = 'localhost';
 const port = 3000;
 
-new WebpackDevServer(webpack(config), {
+const server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
@@ -17,5 +20,24 @@ new WebpackDevServer(webpack(config), {
   },
 }).listen(port, host, function(err, result) {
   if (err) console.log(err);
-  console.log(`Listening at http://${ host }:${ port }`);
+  console.log(`Dev Server Listening at http://${ host }:${ port }`);
+});
+
+jsonServer.listen(8080, () =>
+  console.log(`API Server Listening at http://${ host }:8080`)
+);
+jsonServer.get('/forecast/:latlong', (req, res, next) => {
+  const latlong = req.params.latlong;
+  if (typeof latlong !== 'string' || latlong.indexOf(',') === -1) {
+    const err = new Error('Invalid Coordinates.');
+    err.statusCode = 400;
+    next(err);
+    return;
+  }
+  res.append('Access-Control-Allow-Origin', '*');
+  request(
+    `https://api.darksky.net/forecast/${ process.env.API_KEY }/${
+      req.params.latlong
+    }`
+  ).pipe(res);
 });
